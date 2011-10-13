facd4c3995470189739ac9c8cab9bac2d2e1df35
bm2011 <maas@eventundsport.de>
2011-10-13 09:26:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pioneer::Application.config.secret_token = 'cd61334bd0ff5ead79348b48e0a36f9796a768b639797bff37d24022833b89909d7f3f945269278df94f30a15fa49aa38f86dc285d13571b86bf6ac8580cf01b'
